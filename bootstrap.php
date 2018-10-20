8510f4f2bd9b9fe950e226e593a6759c4f4ad6ae
Ondrej Mirtes <ondrej@mirtes.cz>
2018-10-20 18:42:37 +0200

    Fix for autoloading problem in 0.10.4 (based on PHPStan commit 4dfaaac59d6d1af13fddf6d3e961d69744075be6)

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 
-if (extension_loaded('phar')) {
+if (extension_loaded('phar') && !defined('__PHPSTAN_RUNNING__')) {
 	require_once 'phar://' . __DIR__ . '/phpstan.phar/vendor/autoload.php';
 }
