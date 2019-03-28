48134afd38e5bebc753d23fd3099bca702157112
Jack Barton <jack.barton@inviqa.com>
2019-03-28 08:18:56 +0000

    Add a check for phar in the stream wrappers.
    This resolves an issue whereby the phpstan phar would try to be loaded even if
    phar has not been registered as a stream wrapper in PHP.

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 
-if (extension_loaded('phar') && !defined('__PHPSTAN_RUNNING__')) {
+if (in_array('phar', stream_get_wrappers()) && extension_loaded('phar') && !defined('__PHPSTAN_RUNNING__')) {
 	require_once 'phar://' . __DIR__ . '/phpstan.phar/vendor/autoload.php';
 }
